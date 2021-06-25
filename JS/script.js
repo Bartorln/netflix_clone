e2fde4bb410b9f3da0454ee5c6d25c345bef1c37
Bartolomeu Ribeiro <bartolomeu.rln@gmail.com>
2021-06-25 15:55:11 -0300

    script - ALERTA

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+alert("Olá, Tudo bem? Essa página é um símples treinamento. Esta como as outras páginas no meu repositório, estão sujeitas a alterações.")
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
